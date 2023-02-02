83de8f8010a7a7fb7e13906fcd3440782cf03592
Rachel Kroetch <rekroetch@ucdavis.edu>
2023-02-01 17:29:22 -0800

    grid and dates

--- src/App.js
+++ src/App.js
@@ -2,11 +2,36 @@
 import './App.css';
 
 function App() {
+
+  // const findDate = () => {
+
+  // }
+  
+  const monthArray = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+  const dayArray = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+  const d = new Date();
+  let month = monthArray[d.getMonth()];
+  let year = d.getFullYear();
+  let date = d.getDate();
+  let day = dayArray[d.getDay()];
+
+  let gridboxes = new Array(42).fill(0)
+
   return (
     <div className="App">
       <header className="App-header">
-
+        {month + ' ' + year}
       </header>
+      <div className='today'>
+        {`Today is ${day}, ${month} ${date}`}
+      </div>
+      <div className='flexContainer'>
+        {dayArray.map(weekday=><div>{weekday.slice(0,3)}</div>)}
+      </div>
+      <div className='gridContainer'>
+        {gridboxes.map(()=> <div className="gridItem"></div>)}
+      </div>
     </div>
   );
 }
